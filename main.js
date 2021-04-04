2c40c431d8bff9ee5486c2ae1a9617cf5ab43940
alice9398 <sjs939883@gmail.com>
2021-04-04 22:14:16 +0900

    20210404 commit

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const toggleBtn = document.querySelector('.navbar__toggleBtn');
 const menu = document.querySelector('.navbar__menu');
+const list = document.querySelector('.navbar__menu li ul');
 const icons = document.querySelector('.navbar__icons');
 
 toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
   icons.classList.toggle('active');
+});
+
+
+menu.addEventListener('click', () => {
+    list.classList.toggle('active');
+   // icons.classList.toggle('active');
 });
