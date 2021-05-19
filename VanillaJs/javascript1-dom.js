60c0afb408d1eddb04fc83dbbc4ea629ff3edf10
alice9398 <sjs939883@gmail.com>
2021-05-20 00:35:48 +0900

    childNodes, children

--- VanillaJs/javascript1-dom.js
+++ VanillaJs/javascript1-dom.js
@@ -1,4 +1,16 @@
 ﻿
+
+//Ex4 : ChildNodes를 이용한 노드 선택
+window.addEventListener("load", function () {
+    var section4 = document.querySelector("#section4");
+    var box = section4.querySelector(".box");
+    var input1 = box.children[0];//.childNodes[0];
+    var input2 = box.children[1];
+
+    input1.value = "hello";
+    input2.value = "hi";
+});
+
 //Ex3 : Selectors API LEVEL1
 window.addEventListener("load", function () {
     var section3 = document.getElementById("section3");
