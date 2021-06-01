084198c42f19e4a99f853d62393bd30c5696317e
alice9398 <sjs939883@gmail.com>
2021-06-01 23:08:02 +0900

    파일 정리

--- VanillaJs/js/javascript1-dom.js
+++ VanillaJs/js/javascript1-dom.js
@@ -1,47 +1,4 @@
-﻿
-//Ex11 - 연습문제1 - 선택된 레코드 삭제하기 : event target
-window.addEventListener("load", function () {
-
-    var section = document.querySelector("#section11");
-    var delButton = section.querySelectorAll(".del-button2");
-    for (var i = 0; i < delButton.length; i++) {
-        delButton[i].onclick = function (e) {
-            var tr = e.target.parentElement.parentElement;
-            tr.remove();
-        }
-    }
-
-});
-
-//Ex1 : 선택된 이미지 보여주기 : event target
-
-window.addEventListener("load", () => {
-    var section = document.querySelector("#section1");
-    var imgs = section.querySelectorAll(".img");
-    var currentImg = section.querySelector(".current-img");
-
-    for (var i = 0; i < imgs.length; i++) {
-        imgs[i].onclick = function (e) {
-            currentImg.src = e.target.src;
-        }
-    }
-
-    /* imgs[0].onclick = function (e) {
-        currentImg.src = e.target.src;
-    }
-
-    imgs[1].onclick = function (e) {
-        currentImg.src = e.target.src;
-    }
-
-    imgs[2].onclick = function (e) {
-        currentImg.src = e.target.src;
-    } */
-
-
-});
-
-//Ex10 - 다중 노드선택 방법과 일괄삭제, 노드의 자리 바꾸기
+﻿//Ex10 - Ex10 - 클릭한 컬럼을 기준으로 레코드 정렬하기 #1
 window.addEventListener("load", function () {
 
     var notices = [
---
+++ VanillaJs/js/javascript2-dom.js
@@ -0,0 +1,42 @@
+﻿
+//Ex11 - 연습문제1 - 선택된 레코드 삭제하기 : event target
+window.addEventListener("load", function () {
+
+    var section = document.querySelector("#section11");
+    var delButton = section.querySelectorAll(".del-button2");
+    for (var i = 0; i < delButton.length; i++) {
+        delButton[i].onclick = function (e) {
+            var tr = e.target.parentElement.parentElement;
+            tr.remove();
+        }
+    }
+
+});
+
+//Ex1 : 선택된 이미지 보여주기 : event target
+
+window.addEventListener("load", () => {
+    var section = document.querySelector("#section1");
+    var imgs = section.querySelectorAll(".img");
+    var currentImg = section.querySelector(".current-img");
+
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].onclick = function (e) {
+            currentImg.src = e.target.src;
+        }
+    }
+
+    /* imgs[0].onclick = function (e) {
+        currentImg.src = e.target.src;
+    }
+
+    imgs[1].onclick = function (e) {
+        currentImg.src = e.target.src;
+    }
+
+    imgs[2].onclick = function (e) {
+        currentImg.src = e.target.src;
+    } */
+
+
+});
